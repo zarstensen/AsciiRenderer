73ad0413fd8a03c7a1c1729ebf55159324ebbbbf
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-03-14 11:43:28 +0100

    updated includes

--- src/Asciir.h
+++ src/Asciir.h
@@ -1,8 +1,16 @@
 #pragma once
 
-#include "Asciir/Core.h"
-#include "Asciir/Terminal.h"
+// Core
+#include "Asciir/Core/Core.h"
+#include "Asciir/Core/Terminal.h"
 
 // Entry point
-#include "Asciir/EntryPoint.h"
+#include "Asciir/Core/EntryPoint.h"
 // Entry Point
+
+#include "Asciir/Core/Timing.h"
+// Core
+
+// Logging
+#include "Asciir/Logging/FileLog.h"
+// Logging
