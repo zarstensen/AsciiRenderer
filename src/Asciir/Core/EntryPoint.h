2636d237e04821e7e9ca18e2001bf2c55b90af8a
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-03-16 22:28:25 +0100

    added args argument

--- src/Asciir/Core/EntryPoint.h
+++ src/Asciir/Core/EntryPoint.h
@@ -3,11 +3,17 @@
 #ifdef AR_WIN
 
 // Define this function in your program and return a heap allocated class derived from Asciir::Terminal
-extern Asciir::Terminal* Asciir::CreateTerminal();
+extern Asciir::Terminal* Asciir::CreateTerminal(std::vector<std::string> args);
 
 int main(int argc, char** argv)
 {
-	Asciir::Terminal* terminal = Asciir::CreateTerminal();
+	std::vector<std::string> args(argc);
+	for (size_t i = 0; i < argc; i++)
+	{
+		args[i] = std::move(argv[i]);
+	}
+
+	Asciir::Terminal* terminal = Asciir::CreateTerminal(std::move(args));
 	terminal->start();
 	delete terminal;
 }
