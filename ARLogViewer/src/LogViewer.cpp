7242fce745bb9979e5b95ab83ca6f81d7f933801
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-03-17 22:27:56 +0100

    removed debug log

--- ARLogViewer/src/LogViewer.cpp
+++ ARLogViewer/src/LogViewer.cpp
@@ -74,14 +74,15 @@ namespace Asciir
 		size_t line_indx = log_view.find(']', type_indx + 1);
 		size_t file_indx = log_view.find(']', line_indx + 1);
 
-		std::cout << time_indx << ',' << type_indx << ',' << line_indx << ',' << file_indx << '\n';
-
 		m_log_attributes.setForeground(m_colors[level]);
 		stream << m_log_attributes << log.substr(level_indx + 2, time_indx - level_indx);
+		
 		m_log_attributes.setAttribute(BOLD, true);
 		stream << m_log_attributes << log.substr(time_indx + 3, type_indx - time_indx - 3);
 		m_log_attributes.setAttribute(BOLD, false);
+		
 		stream << m_log_attributes << log.substr(type_indx + 1);
+		
 		m_log_attributes.clear();
 		stream << m_log_attributes;
 
