1f9ff92531ba5542207e2a8fab09de092362896c
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-03-30 17:16:13 +0200

    added events and TerminalRender

--- src/Asciir.h
+++ src/Asciir.h
@@ -4,17 +4,22 @@
 
 // Core
 #include "Asciir/Core/Core.h"
+#include "Asciir/Core/Engine.h"
 #include "Asciir/Core/Terminal.h"
-
-
 #include "Asciir/Core/Timing.h"
 // Core
 
 #include "Asciir/Logging/Log.h"
 #include "Asciir/Logging/FileLog.h"
 
+#include "Asciir/Event/KeyEvent.h"
+#include "Asciir/Event/MouseEvent.h"
+#include "Asciir/Event/TerminalEvent.h"
+
+#include "Asciir/Rendering/TerminalRender.h"
 #include "Asciir/Rendering/AsciiAttributes.h"
 #include "Asciir/Rendering/RenderConsts.h"
+
 
 // Entry point
 #include "Asciir/Core/EntryPoint.h"
