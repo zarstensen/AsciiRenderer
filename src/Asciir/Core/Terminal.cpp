0a4fe31e130eee396a2ad8bbeec6c9a1ea1e84fb
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-04-26 21:23:26 +0200

    implemented terminal

--- src/Asciir/Core/Terminal.cpp
+++ src/Asciir/Core/Terminal.cpp
@@ -1,7 +1,47 @@
 #include "arpch.h"
 #include "Terminal.h"
+#include "Asciir\Logging\Log.h"
+
+#ifdef AR_WIN
+#include "Asciir/Platform/Windows/Core/WindowsTerminal.h"
+#endif
 
 namespace Asciir
 {
+	Terminal::Terminal(const TerminalProps& props)
+		: m_terminal_render(props.title, props.bufferSize)
+	{
+		m_terminal_render.resize(props.size);
+	}
 
+	std::unique_ptr<Terminal> Terminal::create(const TerminalProps& props)
+	{
+		#ifdef AR_WIN
+		return std::make_unique<WindowsTerminal>(props);
+		#else
+		assert((false, "Platform not supported"));
+		#endif
+	}
+
+	
+	
+	void Terminal::onStart()
+	{
+		AR_INFO("Started engine");
+	}
+
+	TermVert Terminal::getSize() const
+	{
+		return m_terminal_render.size();
+	}
+
+	Coord Terminal::getPos() const
+	{
+		return m_terminal_render.pos();
+	}
+
+	const TerminalRender* const Terminal::getRender()
+	{
+		return &m_terminal_render;
+	}
 }
