d074a0d355045e170375aa58aeb3bb6562df95fb
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-04-26 21:26:27 +0200

    added functions to change log dir during runtime

--- src/Asciir/Logging/Log.cpp
+++ src/Asciir/Logging/Log.cpp
@@ -3,9 +3,46 @@
 
 namespace Asciir
 {
-	void Log::Init(bool save_core, bool save_client, bool append_logs)
+	void Log::init(bool save_core, bool save_client, bool append_logs)
 	{
-		s_core_log_out.open(CORE_LOG_DIR.string(), save_core, append_logs);
-		s_client_log_out.open(CLIENT_LOG_DIR.string(), save_client, append_logs);
+		std::filesystem::create_directories(std::filesystem::path(Asciir::CORE_LOG_DIR).parent_path());
+		std::filesystem::create_directories(std::filesystem::path(Asciir::CLIENT_LOG_DIR).parent_path());
+
+		s_core_log_out.open(CORE_LOG_DIR, save_core, append_logs);
+		s_client_log_out.open(CLIENT_LOG_DIR, save_client, append_logs);
+	}
+	
+	void Log::setCoreLogDir(const std::string& dir)
+	{
+		std::filesystem::path path = dir;
+
+		if (path.has_parent_path() && !std::filesystem::exists(path.parent_path())) {
+			AR_VERIFY(std::filesystem::create_directories(std::filesystem::path(dir).parent_path()));
+		}
+
+		s_core_log_out.close();
+		s_core_log_out.open(dir);
+	}
+
+	void Log::setClientLogDir(const std::string& dir)
+	{
+		std::filesystem::path path = dir;
+
+		if (path.has_parent_path() && !std::filesystem::exists(path.parent_path())) {
+			AR_VERIFY(std::filesystem::create_directories(path.parent_path()));
+		}
+		
+		s_client_log_out.close();
+		s_client_log_out.open(dir);
+	}
+
+	const FileLog& Log::getCoreLog()
+	{
+		return s_core_log_out;
+	}
+
+	const FileLog& Log::getClientLog()
+	{
+		return s_client_log_out;
 	}
 }
