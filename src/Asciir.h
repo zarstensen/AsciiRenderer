1b0eeee40049f5b0accf6debd8afa72b3ec8d2e6
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-05-20 20:50:01 +0200

    added AsciirHeader file that does not include the entry point

--- src/Asciir.h
+++ src/Asciir.h
@@ -1,35 +1,8 @@
 #pragma once
+// Includes all of the headers from AsciirHeaders.h
+// and also includes the entry point
 
-#include "pch/arpch.h"
-
-#include "Asciir/Platform/PlatformMacros.h"
-
-// Core
-#include "Asciir/Core/Core.h"
-#include "Asciir/Core/Engine.h"
-#include "Asciir/Core/Terminal.h"
-#include "Asciir/Core/Timing.h"
-#include "Asciir/Core/Layer.h"
-// Core
-
-#include "Asciir/Logging/Log.h"
-#include "Asciir/Logging/FileLog.h"
-
-#include "Asciir/Input/Input.h"
-#include "Asciir/Input/KeyCodes.h"
-
-#include "Asciir/Event/KeyEvent.h"
-#include "Asciir/Event/MouseEvent.h"
-#include "Asciir/Event/TerminalEvent.h"
-
-#include "Asciir/Rendering/TerminalRender.h"
-#include "Asciir/Rendering/AsciiAttributes.h"
-#include "Asciir/Math/Vertices.h"
-#include "Asciir/Math/Matrix.h"
-#include "Asciir/Math/Tensor.h"
-#include "Asciir/Math/Lines.h"
-
-#include "Asciir/Rendering/RenderConsts.h"
+#include "AsciirHeaders.h"
 
 // Entry point
 #include "Asciir/Core/EntryPoint.h"
