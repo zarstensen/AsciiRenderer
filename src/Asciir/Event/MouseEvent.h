eaeb2c52c2156434bb21dff12f51687a49d52fd8
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-05-21 15:24:28 +0200

    added cursor pos to MouseEvents

--- src/Asciir/Event/MouseEvent.h
+++ src/Asciir/Event/MouseEvent.h
@@ -11,14 +11,18 @@ namespace Asciir
 	{
 	protected:
 		MouseKey m_mousecode;
+		Coord m_mouse_pos;
+		TermVert m_cur_pos;
 
 	protected:
-		MouseKeyEvent(MouseKey mousecode)
-			: m_mousecode(mousecode) {}
+		MouseKeyEvent(MouseKey mousecode, Coord mouse_pos, TermVert cur_pos)
+			: m_mousecode(mousecode), m_mouse_pos(mouse_pos), m_cur_pos(cur_pos) {}
 
 	public:
 
 		MouseKey getMouseCode() const { return m_mousecode; }
+		Coord getMousePos() const { return m_mouse_pos;  }
+		TermVert getCursorPos() const { return m_cur_pos; }
 
 		EVENT_CATEGORY_DEFINE(CategoryInput | CategoryMouse)
 
@@ -28,8 +32,8 @@ namespace Asciir
 	{
 		friend Input;
 	protected:
-		MousePressedEvent(MouseKey mousecode)
-			: MouseKeyEvent(mousecode) {}
+		MousePressedEvent(MouseKey mousecode, Coord mouse_pos, TermVert cur_pos)
+			: MouseKeyEvent(mousecode, mouse_pos, cur_pos) {}
 
 	public:
 
@@ -49,8 +53,8 @@ namespace Asciir
 	{
 		friend Input;
 	protected:
-		MouseReleasedEvent(MouseKey mousecode)
-			: MouseKeyEvent(mousecode) {}
+		MouseReleasedEvent(MouseKey mousecode, Coord mouse_pos, TermVert cur_pos)
+			: MouseKeyEvent(mousecode, mouse_pos, cur_pos) {}
 
 	public:
 
@@ -71,19 +75,24 @@ namespace Asciir
 	protected:
 		Coord m_pos;
 		Coord m_diff;
+		TermVert m_cur_pos;
+		TermVert m_cur_diff;
+
 	public:
-		MouseMovedEvent(Coord pos, Coord diff)
-			: m_pos(pos), m_diff(diff)
+		MouseMovedEvent(Coord pos, Coord diff, TermVert cur_pos, TermVert cur_diff)
+			: m_pos(pos), m_diff(diff), m_cur_pos(cur_pos), m_cur_diff(cur_diff)
 		{}
 
 		Coord getPos() const { return m_pos; }
 		Coord getDiff() const { return m_diff; }
+		TermVert getCursorPos() const { return m_cur_pos; }
+		TermVert getCursorDiff() const { return m_cur_diff;  }
 
 		virtual std::string toString() const override
 		{
 			std::stringstream msg;
 
-			msg << "MouseMoved: " << getPos() << ':' << getDiff();
+			msg << "MouseMoved: " << getPos() << ':' << getDiff() << "   " << getCursorPos() << ':' << getCursorDiff();
 			
 			return msg.str();
 		}
