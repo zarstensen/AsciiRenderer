9872aabfc4c9fb3a464f565f93a4f3f2447c3793
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-05-27 23:14:09 +0200

    implemented WinEventListener

--- src/Asciir/Input/EventListener.h
+++ src/Asciir/Input/EventListener.h
@@ -1,54 +1,76 @@
 #pragma once
 
 #include "Asciir/Core/Timing.h"
-#include "Asciir/Core/Terminal.h"
+#include "Asciir/Event/Event.h"
+#include "Asciir/Input/KeyCodes.h"
+
+#include "Asciir/Math/Vertices.h"
 
 namespace Asciir
 {
+
 	class EventListener
 	{
-	protected:
-		const Terminal::EventCallbackFp& m_callback;
+	public:
+		using EventCallbackFp = std::function<void(Event&)>;
+		
 
-		// structure that holds key data and times key down events
+		// structures that holds key data and times key down events
 		struct KeyInputData
 		{
 			bool is_down = false;
-			bool is_toggled = false;
+			bool is_repeat = false;
+
+			duration time_since_down = duration();
+		};
+
+		struct MouseInputData
+		{
+			bool is_down = false;
+			bool is_double_click = false;
 
 			duration time_since_down = duration();
 		};
 
 		// size of key input array for the keyboard
-		static constexpr size_t KIS_LEN = 256;
+		static constexpr size_t KIS_LEN = KEY_CODE_COUNT;
 		// size of key input array for the mouse
-		static constexpr size_t MIS_LEN = 5;
-
+		static constexpr size_t MIS_LEN = MOUSE_CODE_COUNT;
+	
+	protected:
+		EventCallbackFp m_callback;
 		// updated for every event
 		std::array<KeyInputData, KIS_LEN> keybd_state;
 		// copies and store keybd_state every pollUpdate
 		std::array<KeyInputData, KIS_LEN> keybd_poll_state;
 
 		// same for mouse
-		std::array<KeyInputData, MIS_LEN> mouse_state;
-		std::array<KeyInputData, MIS_LEN> mouse_poll_state;
+		std::array<MouseInputData, MIS_LEN> mouse_state;
+		std::array<MouseInputData, MIS_LEN> mouse_poll_state;
+
+		Coord m_last_mouse_pos;
+		TermVert m_last_cur_pos;
 
 	public:
 
-		EventListener() = default;
-		virtual ~EventListener() = 0;
+		EventListener() {};
+		virtual ~EventListener() {};
 
-		virtual void start(const Terminal::EventCallbackFp& callback) = 0;
+		virtual void start(EventCallbackFp callback) = 0;
 		virtual void stop() = 0;
 
 		void pollState()
 		{
-			std::copy(keybd_state.begin(), keybd_state.end(), keybd_poll_state);
-			std::copy(mouse_state.begin(), mouse_state.end(), mouse_poll_state);
+			keybd_poll_state = keybd_state;
+			mouse_poll_state = mouse_state;
 		};
 
 		const std::array<KeyInputData, KIS_LEN>& getKeybdPoll() { return keybd_poll_state; }
-		const std::array<KeyInputData, MIS_LEN>& getMousePoll() { return mouse_poll_state; }
+		const std::array<MouseInputData, MIS_LEN>& getMousePoll() { return mouse_poll_state; }
 		
+		Coord getLastMousePos();
+		TermVert getLastCursorPos();
+
+		static Coord getMousePos();
 	};
 }
