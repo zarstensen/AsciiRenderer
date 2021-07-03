72d9a7c7cfb4b0a6fe97fe75dd457c10fe18e1fc
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-07-03 21:27:10 +0200

    implemented delta_time into the example

--- Examples/BasicInputExample/src/Executable.cpp
+++ Examples/BasicInputExample/src/Executable.cpp
@@ -3,26 +3,31 @@
 class ExampleLayer : public Asciir::Layer
 {
 	Asciir::Coord pos = { 10, 10 };
+	static constexpr Asciir::Real speed = 0.025;
+
+	void onStart()
+	{
+		Asciir::Renderer::setMinDT(Asciir::DeltaTime::FPS(60));
+	}
 
 	void onUpdate(Asciir::DeltaTime delta_time) final
 	{
 		Asciir::Renderer::clear(Asciir::Tile({ 0, 0, 155 }, {0, 0, 155}, 219));
 
+
 		if (Asciir::Input::isKeyDown(Asciir::Key::W) || Asciir::Input::isKeyDown(Asciir::Key::UP))
-			pos.y--;
+			pos.y -= delta_time * speed;
 
 		if (Asciir::Input::isKeyDown(Asciir::Key::S) || Asciir::Input::isKeyDown(Asciir::Key::DOWN))
-			pos.y++;
+			pos.y += delta_time * speed;
 
 		if (Asciir::Input::isKeyDown(Asciir::Key::A) || Asciir::Input::isKeyDown(Asciir::Key::LEFT))
-			pos.x--;
+			pos.x -= delta_time * speed;
 
 		if (Asciir::Input::isKeyDown(Asciir::Key::D) || Asciir::Input::isKeyDown(Asciir::Key::RIGHT))
-			pos.x++;
+			pos.x += delta_time * speed;
 
 		Asciir::Renderer::submitTile(pos, Asciir::Tile(0, { (u_char)rand(), (u_char)rand(), (u_char)rand() }, 219));
-
-		Asciir::sleep(1000 / 60);
 	}
 };
 
