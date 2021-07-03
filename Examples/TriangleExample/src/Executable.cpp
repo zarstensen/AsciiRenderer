e506c43f380fc4a3db6b6f77e7d4acaca461af34
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-07-03 17:57:43 +0200

    updated to use the new multithreaded system

--- Examples/TriangleExample/src/Executable.cpp
+++ Examples/TriangleExample/src/Executable.cpp
@@ -1,28 +1,28 @@
 #include <Asciir.h>
 
+using namespace Asciir::AsciirLiterals;
+
 class TriangleLayer : public Asciir::Layer
 {
 
-	Asciir::Mesh triangle_verts = Asciir::Mesh(
+	Asciir::Ref<Asciir::Mesh> triangle_verts = Asciir::Mesh(
 		{ { {20, 40}, {50, 10}, {80, 40} },
 		  { {35, 25}, {65, 25}, {50, 40} } });
+
+	Asciir::Ref<Asciir::Mesh> square_mesh = Asciir::Mesh({ {0, 0}, { 3, 0 }, {3, 3}, {0, 3} });
 
 	Asciir::TInt pos = 0;
 	bool forward = true;
 
-	Asciir::Real avg_time = 0;
-	size_t frames = 0;
-
 	void onStart() final
 	{
 		Asciir::Renderer::resize({ 240, 63 });
+		Asciir::Renderer::setMinDT(Asciir::DeltaTime::FPS(60));
 	}
 
 	void onUpdate(Asciir::DeltaTime delta_time) final
 	{
-		avg_time += delta_time.seconds();
-		frames++;
-		AR_INFO(1/(avg_time/frames));
+		AR_INFO(delta_time);
 
 		Asciir::Renderer::clear(Asciir::Tile(25));
 		
@@ -31,14 +31,26 @@ class TriangleLayer : public Asciir::Layer
 		else
 			pos--;
 
-		if (pos > 180 || pos <= -80)
+		if (pos > 180 || pos <= 0)
 			forward = !forward;
 
-		Asciir::Renderer::drawMesh(triangle_verts.offset({ forward ? 1 : -1, 0 }), Asciir::Tile(Asciir::YELLOW8));
+		Asciir::Transform transform;
+		Asciir::Transform square_transform;
+
+		square_transform.pos.x = pos;
+		square_transform.pos.y = 0;
+		square_transform.origin = square_mesh->getMedianVert();
+		Asciir::Renderer::submitMesh(square_mesh, Asciir::Tile(Asciir::IRED8), square_transform);
+		
+		transform.pos.x = pos;
+		transform.pos.y = 0;
+		transform.scale.x = 1;
+		transform.origin = triangle_verts->getMedianVert();
+		transform.rotation = Asciir::degToRad(pos + 45);
+
+		Asciir::Renderer::submitMesh(triangle_verts, Asciir::Tile(Asciir::YELLOW8), transform);
 
 		Asciir::Renderer::resize({ 240, 63 });
-
-		Asciir::sleep(1000 / 60);
 	}
 };
 
@@ -50,12 +62,6 @@ public:
 		PushLayer(new TriangleLayer);
 	}
 };
-
-std::ostream& operator<<(std::ostream& stream, bool boolean_value)
-{
-	stream << (boolean_value ? "true" : "false");
-	return stream;
-}
 
 Asciir::AREngine* Asciir::createEngine(std::vector<std::string> args)
 {
