9cc554f190e67939d7c3ec4fbec73264a8bc99e5
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-08-23 21:41:37 +0200

    update example

--- Examples/TextureExample/src/Executable.cpp
+++ Examples/TextureExample/src/Executable.cpp
@@ -2,10 +2,29 @@
 
 using namespace Asciir;
 
+class TileShader : public Shader2D
+{
+
+	Size2D size() const final override
+	{
+		return Renderer::size();
+	}
+
+	Tile readTile(const Size2D& coord, const DeltaTime&, const size_t& frame) const final override
+	{
+		//srand(rand() % 1000 + frame);
+		if((coord.x + coord.y) % 2)
+			return Tile(BLACK8);
+		else
+			return Tile(IWHITE8);
+	}
+};
+
 class TextureLayer : public Asciir::Layer
 {
 	Ref<FileTexture> texture = FileTexture();
-	const Path texture_path = "./texture.cart";
+	Ref<TileShader> shader = TileShader();
+	const Path texture_path = "./test.cart";
 
 	void onAdd() final
 	{
@@ -18,9 +37,10 @@ class TextureLayer : public Asciir::Layer
 	{
 		Transform texture_t;
 		texture_t.pos = { 10, 10 };
-		texture_t.scale = { 2, 2 };
+		texture_t.scale = { 1, 1 };
 		texture_t.origin = (Coord)texture->size() / 2;
 
+		Renderer::submitShader(shader);
 		Renderer::submitShader(texture, texture_t);
 		//Renderer::resize({ 20, 20 });
 	}
