7efd879d243b2a31d6457cc734d55b5d4f5dd16f
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-09-09 20:11:21 +0200

    implemented componentbuffer

--- src/Asciir/Entities/ECS.cpp
+++ src/Asciir/Entities/ECS.cpp
@@ -4,4 +4,95 @@
 namespace Asciir
 {
 
+    /*
+    * ComponentBuffer definitions
+    */
+
+    uint8_t* ComponentBuffer::get(size_t indx)
+    {
+        AR_ASSERT_MSG(hasIndex(indx), "Invalid index was passed: ", indx, "\nSize: ", getSize(), "\nData length", dataLength());
+        return &m_data[mapIncoming(indx) * (m_data_size + sizeof(size_t))];
+    }
+
+    void ComponentBuffer::set(size_t indx, uint8_t* data)
+    {
+        if (!hasIndex(indx))
+        {
+            m_data.reserve(m_data.size() + m_data_size + sizeof(size_t));
+
+            m_data.insert(m_data.end(), data, data + m_data_size);
+            m_data.insert(m_data.end(), (uint8_t*) &indx, (uint8_t*) &indx + sizeof(size_t));
+
+            m_sparse_map[indx] = dataLength() - 1;
+        }
+        else
+        {
+            auto begin = m_data.begin() + mapIncoming(indx) * (m_data_size + sizeof(size_t));
+            std::copy(data, data + m_data_size, begin);
+        }
+    }
+
+    // assumes all elements outside of the new range has been removed
+    // if these should automatticly be removed, call resize instead.
+    void ComponentBuffer::setSize(size_t size)
+    {
+    #ifdef AR_DEBUG
+        for (size_t i = size; i < getSize(); i++)
+            AR_ASSERT_MSG(!hasIndex(i), "setSize was called with elements outside the new size range\nNew size: ", size, "\nCurrent size: ", getSize(), "Element index: ", i);
+    #endif
+
+        m_sparse_map.resize(size);
+    }
+
+    void ComponentBuffer::resize(size_t size)
+    {
+        for (size_t i = size; i < getSize(); i++)
+            if(hasIndex(i)) remove(i);
+        
+        setSize(size);
+    }
+
+    bool ComponentBuffer::hasIndex(size_t indx)
+    {
+        for (size_t i = 0; i < dataLength(); i++)
+            if (mapOutgoing(i) == indx) return true;
+        
+        return false;
+    }
+
+    size_t ComponentBuffer::mapOutgoing(size_t indx)
+    {
+        // go to the start of the target component and offset by component size to get the index relative to the sparse map
+        return *(size_t*) &m_data[indx * (m_data_size + sizeof(size_t)) + m_data_size];
+    }
+
+    void ComponentBuffer::remove(size_t indx)
+    {
+        // offset all indexes past the element to the left in the sparse map
+        for (size_t i = mapIncoming(indx) + 1; i < dataLength(); i++)
+            m_sparse_map[mapOutgoing(i)]--;
+         
+        // remove the component from component data vector
+        auto begin = m_data.begin() + mapIncoming(indx) * (m_data_size + sizeof(size_t));
+        auto end = begin + (m_data_size + sizeof(size_t));
+        m_data.erase(begin, end);
+    }
+    
+    /*
+    * System definitions
+    */
+
+    void System::run()
+    {
+        for (Ref<Scene> scene : m_system_scenes)
+        {
+
+
+        }
+    }
+
+    void System::addScene(Ref<Scene> scene)
+    {
+    }
+
 }
--- src/Asciir/Entities/ECS.h
+++ src/Asciir/Entities/ECS.h
@@ -8,8 +8,8 @@ namespace Asciir
 	using UID = size_t;
 
 	static constexpr UID INVALID_ENTITY_UID = 0;
-
 	
+
 	// a class that can be associated with an entity and represents some data about said entity
 	// should only hold data and should not be associated with any functionallity
 	// acts as a base class for user and predefined components
@@ -19,12 +19,15 @@ namespace Asciir
 		Component(UID uid = INVALID_ENTITY_UID)
 			: m_entity_id(uid) {}
 
+
 		UID getEntUID() const { return m_entity_id; }
+
+		// cast operator to implicitly cast when adding a component to a component buffer
+		operator uint8_t*() { return (uint8_t*) this; }
 
 	protected:
 		UID m_entity_id;
 	};
-
 
 	template<typename TComp>
 	using enable_if_component = enable_if_base_of<TComp, Component>;
@@ -36,11 +39,13 @@ namespace Asciir
 		friend System;
 	
 		UID getEntityID() { return m_entity_id; }
+
 		template<typename TComp, enable_if_component<TComp> = false>
-		Ref<TComp> getComponent();
+		Ref<TComp> getComponent() { return m_components[typeid(TComp)]; }
 
 	protected:
-		SystemEvent(std::map<std::type_index, Ref<Component>>& components, UID entity);
+		SystemEvent(std::map<std::type_index, Ref<Component>>& components, UID entity)
+			: m_components(components), m_entity_id(entity) {}
 
 		std::map<std::type_index, Ref<Component>>& m_components;
 		UID m_entity_id;
@@ -51,9 +56,19 @@ namespace Asciir
 
 	class Scene;
 
-	// a class that reads and modifies one or more components.
-	// can alert other systems via SystemEvents.
-	// acts as a base class for all user and library predefined systems.
+	/*
+	a class that readsand modifies one or more components.
+	acts as a base class for all user and library predefined systems.
+
+	use createEvent to alert other systems subscribed to a specific SystemEvent.
+
+	use Subscribe and Unsubscribe to define what SystemEvents will notify the system.
+	
+	use requireComponent and unrequireComponent to define what components the system needs to function.
+
+	use addScene and removeScene to define what scenes the system should run on.
+
+	*/
 	class System
 	{
 	public:
@@ -71,11 +86,24 @@ namespace Asciir
 		void run();
 
 		template<typename TComp, enable_if_component<TComp> = false>
-		Ref<TComp> getComponent();
+		Ref<TComp> getComponent() { return m_active_components[typeid(TComp)]; }
 
 		// automaticlly creates and populates an event of specified type and dispatches it to other systems in the associated scenes.
 		template<typename TEvent, enable_if_event<TEvent> = false>
 		void createEvent();
+
+		// if multiple components are required at once, the unordered_set will be resized once instead of every time a new component is inserted
+		template<typename First, typename ... Args>
+		void requireComponents();
+
+		template<typename TComp, enable_if_component<TComp>>
+		void requireComponents();
+
+		template<typename First, typename ... TComps>
+		void unrequireComponents();
+
+		template<typename TComp, enable_if_component<TComp>>
+		void unrequireComponents();
 
 		// asocciates a scene with the system
 		void addScene(Ref<Scene> scene);
@@ -89,10 +117,16 @@ namespace Asciir
 
 	protected:
 
+		template<typename First, typename ... Args>
+		void requireComponentsHelper();
+
+		template<typename TComp>
+		void requireComponentsHelper() { requireComponents<TComp>(); }
+
 		// what components the system needs to function
-		std::unordered_set<UID> m_system_components;
+		std::unordered_set<std::type_index> m_system_components;
 		// what scenes the system is a part of
-		std::vector<Ref<Scene>> m_system_scenes;
+		std::unordered_set<Ref<Scene>> m_system_scenes;
 
 		// stores a reference for each component currently being processed.
 		std::map<std::type_index, Ref<Component>> m_active_components;
@@ -104,19 +138,43 @@ namespace Asciir
 	class ComponentBuffer
 	{
 	public:
-		ComponentBuffer(size_t data_size);
+		ComponentBuffer(size_t data_size) : m_data_size(data_size) {}
 
 		uint8_t* get(size_t indx);
 		void remove(size_t indx);
-		void append(uint8_t* data);
+		// if a component already is stored at the current index, it is overwritten
+		void set(size_t indx, uint8_t* data);
 
-		uint8_t* operator[](size_t indx);
+		// set the size of the sparse map without affecting the size of the data array
+		void setSize(size_t size);
+		// gets the size of the sparse map
+		size_t getSize() { return m_sparse_map.size(); }
+
+		// sets the size of the sparse map and removes any elements not instide the new size
+		void resize(size_t size);
+
+		// gets the number of components currently stored in the component buffer
+		size_t dataLength() { return m_data.size() / (m_data_size + sizeof(size_t)); }
+
+
+		// check wether the index passed has an element associated with the ComponentBuffer
+		bool hasIndex(size_t indx);
+
+		// maps an incomming index using the sparse map
+		size_t mapIncoming(size_t indx) { return m_sparse_map[indx]; }
+		
+		// maps an index relative to m_data to the sparse_map
+		size_t mapOutgoing(size_t indx);
+
+		uint8_t* operator[](size_t indx) { return get(indx); }
 
 	protected:
 
 		size_t m_data_size;
+		// organized as a single element being stored as [Component part (unknown bytes) + size part (4 / 8 bytes)]
 		std::vector<uint8_t> m_data;
-
+		
+		std::vector<size_t> m_sparse_map;
 	};
 
 	// class representing a collection of entities and systems.
@@ -159,3 +217,5 @@ namespace Asciir
 
 	};
 }
+
+#include "ECS.ipp"
