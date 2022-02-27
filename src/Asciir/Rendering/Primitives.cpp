122f859ce96e85b04e8336680656793444d53637
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2022-02-27 15:11:19 +0100

    implemented quad primitive

---
+++ src/Asciir/Rendering/Primitives.cpp
@@ -0,0 +1,29 @@
+#include "Primitives.h"
+#include "Asciir/Maths/Maths.h"
+
+namespace Asciir
+{
+    Coord Quad::centrePoint(const Transform& transform) const
+    {
+        // simply get the centre of the quad as if it was not transformed, and then reverse the transformation to the resulting point.
+        return transform.applyTransform((size - offset) / 2);
+    }
+
+    bool Quad::isInside(Coord coord, const Transform& transform) const
+    {
+        // first, reverse the transformation on coord.
+        // this makes sure we do not have to transform the quad itself, making the calculations more expensive.
+
+        coord = transform.reverseTransform(coord) - offset;
+
+        return    0 <= coord.x && coord.x <= size.x
+               && 0 <= coord.y && coord.y <= size.y;
+    }
+    
+    bool Quad::isInsideGrid(const Coord& coord, Real resolution, const Transform& transform) const
+    {
+        // simply create a new quad which is fitted to a grid corresponding to the resolution
+        Quad grid_quad(Coord(ceil(size.x, resolution), floor(size.y, resolution)), Coord(floor(offset.x, resolution), ceil(offset.y, resolution)));
+        return grid_quad.isInside(coord, transform);
+    }
+}
---
+++ src/Asciir/Rendering/Primitives.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Asciir/Maths/Vertices.h"
+#include "Mesh.h"
+
+namespace Asciir
+{
+	/// @brief class representing a quad, as well as implementing less expensive functions from the Mesh class.  
+	///
+	/// the quad is stored as the position of the top right corner of the quad, and the size of the quad.
+	/// 
+	class Quad
+	{
+
+	public:
+
+		Coord offset;
+		Coord size;
+		
+		Quad() = default;
+
+		Quad(Coord size, Coord offset = Coord(0, 0))
+			: size(size), offset(offset) {}
+
+
+		/// @brief retrieve the center point of the quad, with the passed transform
+		Coord centrePoint(const Transform& transform = NoTransform) const;
+
+		/// @brief checks if a coordinate is inside the quad with the passed transform applied
+		bool isInside(Coord coord, const Transform& transform = NoTransform) const;
+		/// @brief checks if a coordinate is inside the quad, fitted within a grid with the given resolution, and with the passed transform applied.
+		bool isInsideGrid(const Coord& boord, Real resolution, const Transform& transform = NoTransform) const;
+
+		/// @brief get the 4 vertices constructing the quad represented
+		s_arVertices2D<Real, 4> getVerts()
+		{
+			return { offset, Coord{ offset.x, size.y }, size, Coord{ size.x, offset.y } };
+		}
+
+	};
+
+	/// @brief outputs the offset and the quad size to the passed stream
+	std::ostream& operator<<(std::ostream& stream, const Quad& quad)
+	{
+		stream << quad.offset << ' ' << quad.size;
+	}
+
+}
